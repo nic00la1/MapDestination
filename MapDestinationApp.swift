fc322dab7c1a4171436e43d075d21d9bbb1e9be3
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-28 11:33:40 +0100

    Initial commit

---
+++ MapDestinationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapDestinationApp.swift
+//  MapDestination
+//
+//  Created by Nicola Kaleta on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapDestinationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Models/Location.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  MapDestination
+//
+//  Created by Nicola Kaleta on 28/02/2024.
+//
+
+import Foundation
