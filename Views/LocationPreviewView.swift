95b87cb375282b3281c64c79c536f72de1bf3ece
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-03-02 14:19:54 +0100

    image section ✅

--- Views/LocationPreviewView.swift
+++ Views/LocationPreviewView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct LocationPreviewView: View {
+    // MARK: - Properties
+    @EnvironmentObject private var vm : LocationViewModel
+    let location : Location
+    
+    // MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .bottom, spacing: 0) {
+            VStack(alignment: .leading, spacing: 16) {
+                imageSection
+            }
+        }
     }
 }
 
 #Preview {
-    LocationPreviewView()
+    ZStack {
+        Color.green.ignoresSafeArea()
+        LocationPreviewView(location: LocationDataService.locations.first!)
+            .padding()
+        
+    }
+}
+
+extension LocationPreviewView {
+    
+    // MARK: - Image Section
+    private var imageSection : some View {
+        ZStack {
+            if let imageName = location.imageNames.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+            }
+        }
+        .padding(6)
+        .background(.white)
+        .cornerRadius(10)
+    }
+    
+    
 }
